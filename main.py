d85550a48d19fd77762ddc803463a039d05a2b17
Clydowski <hendrikmueller52@gmail.com>
2025-08-07 18:28:57 +0200

    finished the programm, added how to use

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from stats import get_num_words, get_char_counts, sort_char_counts
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
 
     print("=========== BOOKBOT ============")
